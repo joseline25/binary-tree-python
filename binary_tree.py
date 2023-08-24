fe960ed92366b94ed2ff36c5401f9af3aafded1a
joseline25 <youjoseline@gmail.com>
2023-08-24 17:40:06 +0100

    update with list of nodes of a tree, user class from node class

--- binary_tree.py
+++ binary_tree.py
@@ -192,3 +192,105 @@ print("Degree:", degree)  # the maximum number of sons of a node in the tree
 print("Levels:", levels)
 print("Height:", height)
 print("Path to 5:", path)
+
+
+# parcours de l'arbre à partir des feuilles
+
+
+def traverse_tree_from_external_nodes_to_root(node):
+    if node is None:
+        return
+
+    # Traverse from external nodes to root
+    current = node
+
+    while current.left is not None or current.right is not None:
+        print(current.value)
+
+        if current.right is not None:
+            current = current.right
+        else:
+            current = current.left
+
+    # Print the root value
+    print(current.value)
+
+
+# Traverse the tree from external nodes to root
+print("Traversal from External Nodes to Root:")
+traverse_tree_from_external_nodes_to_root(root)
+
+
+"""
+    Je peux juste itérer à travers les éléments de l'array levels, et pour chacun, je perfor
+    les calculs de chaque noeuds.
+    Il faudra ajouter d'autres propriétés au noeud tels que les éléments de la clé privée
+    et les élements de la clé publique. 
+    
+    ou je crée une classe qui hérite de node et a en plus des proprétés de node, les propriétés
+    dont j'ai besoin.
+
+"""
+
+# la deuxième version de l'algo de construction qui return l'arbre (root)
+# et la liste des noeuds de l'arbre
+
+
+def construct_locally_complete_binary_tree_two(num_leaves):
+    if num_leaves <= 0:
+        return None, []
+
+    root = Node(0)  # the root value is 0
+    queue = [root]
+    leaves_added = 1
+    nodes = [root]
+
+    while leaves_added < num_leaves:
+        current = queue.pop(0)
+
+        # Add left child if there are still leaves to be added
+        if leaves_added < num_leaves:
+            current.left = Node(current.value * 2 + 1)
+            queue.append(current.left)
+            leaves_added += 1
+            nodes.append(current.left)
+
+        # Add right child if there are still leaves to be added
+        if leaves_added < num_leaves:
+            current.right = Node(current.value * 2 + 2)
+            queue.append(current.right)
+            leaves_added += 1
+            nodes.append(current.right)
+
+    return root, nodes
+
+
+# Example usage
+num_leaves = 7
+root, all_nodes = construct_locally_complete_binary_tree_two(num_leaves)
+
+# Printing the values of all nodes in the tree
+print("Les valeurs des noeuds de l'arbre à " +
+      str(num_leaves) + " feuilles sont :")
+for node in all_nodes:
+    print(node.value)
+
+
+# la classe User qui hérite de Node avec en plus les attributs private key et public key
+
+class User(Node):
+    def __init__(self, value, private_key, public_key):
+        super().__init__(value)
+        self.private_key = private_key
+        self.public_key = public_key
+
+
+# Example usage
+user = User(1, 3, 20)
+
+print("User value:", user.value)
+print("Private key:", user.private_key)
+print("Public key:", user.public_key)
+
+def brother(value):
+    pass
